24f3450b0f24ef556be729c88f2c8481cdd6fbb0
AbhijeetRaj <AbhijeetRaj@ABHIJEETRAJ>
2024-09-16 17:04:38 +0530

    Add project files.

---
+++ RegistrationForm/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationForm.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? State { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        [Range(18, 100)]
+        public int Age { get; set; }
+    }
+}
